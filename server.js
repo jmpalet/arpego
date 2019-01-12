48f73e53ac6e679adb44a0ffedca9bd860061bc0
Josep Palet <josep.palet@flixbus.com>
2019-01-12 12:17:05 +0100

    Deploy

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
 app.listen(port);
